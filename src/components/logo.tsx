cd9077127016ade8afbae7425be9c8dc7c8ec27b
solopaw <adamjoelfraser@icloud.com>
2025-05-15 15:53:14 +1000

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/(home)/_components/chats-card.tsx 	new file:   src/app/(home)/_components/overview-cards/card.tsx 	new file:   src/app/(home)/_components/overview-cards/icons.tsx 	new file:   src/app/(home)/_components/overview-cards/index.tsx 	new file:   src/app/(home)/_components/overview-cards/skeleton.tsx 	new file:   src/app/(home)/_components/region-labels/index.tsx 	new file:   src/app/(home)/_components/region-labels/map.tsx 	new file:   src/app/(home)/fetch.ts 	new file:   src/app/(home)/page.tsx 	renamed:    src/assets/logos/dark.svg -> src/assets/logos/dark.svg.tsx 	new file:   src/assets/logos/facebook.svg.tsx 	new file:   src/assets/logos/github.svg.tsx 	new file:   src/assets/logos/google.svg.tsx 	new file:   src/assets/logos/index.ts 	renamed:    src/assets/logos/main.svg -> src/assets/logos/main.svg.tsx 	new file:   src/assets/logos/vimeo.svg.tsx 	new file:   src/assets/logos/x.svg.tsx 	modified:   src/components/logo.tsx 	new file:   src/components/ui/skeleton.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,27 +1,11 @@
-import darkLogo from "@/assets/logos/dark.svg";
-import logo from "@/assets/logos/main.svg";
-import Image from "next/image";
+import DarkLogo from "@/assets/logos/dark.svg";
+import MLogo from "@/assets/logos/main.svg";
 
 export function Logo() {
   return (
     <div className="relative h-8 max-w-[10.847rem]">
-      <Image
-        src={logo}
-        fill
-        className="dark:hidden"
-        alt="NextAdmin logo"
-        role="presentation"
-        quality={100}
-      />
-
-      <Image
-        src={darkLogo}
-        fill
-        className="hidden dark:block"
-        alt="NextAdmin logo"
-        role="presentation"
-        quality={100}
-      />
+      <MLogo fill="" className="dark:hidden" role="presentation" />
+      <DarkLogo fill="" className="hidden dark:block" role="presentation" />
     </div>
   );
 }
