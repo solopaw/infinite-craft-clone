5aab8d0c6d5f776abcb1f7356d4c4c1495aaecb4
solopaw <adamjoelfraser@icloud.com>
2025-05-15 16:21:04 +1000

    new file:   jsvectormap.d.ts 	new file:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/assets/logos/dark.svg.tsx -> src/assets/logos/dark.svg 	renamed:    src/assets/logos/facebook.svg.tsx -> src/assets/logos/facebook.svg 	renamed:    src/assets/logos/github.svg.tsx -> src/assets/logos/github.svg 	renamed:    src/assets/logos/google.svg.tsx -> src/assets/logos/google.svg 	renamed:    src/assets/logos/main.svg.tsx -> src/assets/logos/main.svg 	renamed:    src/assets/logos/vimeo.svg.tsx -> src/assets/logos/vimeo.svg 	renamed:    src/assets/logos/x.svg.tsx -> src/assets/logos/x.svg 	new file:   src/components/Charts/payments-overview/chart.tsx 	new file:   src/components/Charts/payments-overview/index.tsx 	new file:   src/components/Charts/used-devices/chart.tsx 	new file:   src/components/Charts/used-devices/index.tsx 	new file:   src/components/Charts/weeks-profit/chart.tsx 	new file:   src/components/Charts/weeks-profit/index.tsx 	new file:   src/components/Tables/fetch.ts 	new file:   src/components/Tables/top-channels/index.tsx 	new file:   src/components/Tables/top-channels/skeleton.tsx 	new file:   src/components/period-picker.tsx 	new file:   src/components/ui/table.tsx 	new file:   src/lib/format-number.ts 	new file:   src/services/charts.services.tsx 	modified:   tsconfig.json 	new file:   types.d.ts

---
+++ next.config.mjs
@@ -0,0 +1,42 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
+        port: ""
+      }
+    ]
+  },
+  webpack(config){
+    
+    config.module.rules.push({
+      test: /\.(svg)$/,
+      use: [{
+        loader: 'svg-url-loader',
+        options: { limit: 10000 }
+      }]
+    });
+
+    return config;
+  },
+  
+};
+
+export default nextConfig;
