01df294fbb85a47c84186cd4b4522cfe3b255458
solopaw <adamjoelfraser@icloud.com>
2025-05-15 15:08:19 +1000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
     return (
-        <main>
+        <main className="">
         <h1>Welcome to Next.js!</h1>
         <p>This is a simple Next.js application.</p>
         </main>
